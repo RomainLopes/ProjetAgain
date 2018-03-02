0a400dcd787430cfde11f4ad498fe0afd26f1163
Unknown <rome.lopes.student@gmail.com>
2018-03-02 15:59:18 +0100

    gbdd dm

--- ProjetSIH/src/GestionBDD/DossierMedical.java
+++ ProjetSIH/src/GestionBDD/DossierMedical.java
@@ -10,5 +10,79 @@ package GestionBDD;
  * @author romel
  */
 public class DossierMedical {
+
+    /**
+     * @return the ipp
+     */
+    public int[] getIpp() {
+        return ipp;
+    }
+
+    /**
+     * @param ipp the ipp to set
+     */
+    public void setIpp(int[] ipp) {
+        this.ipp = ipp;
+    }
+
+    /**
+     * @return the nosejour
+     */
+    public int[] getNosejour() {
+        return nosejour;
+    }
+
+    /**
+     * @param nosejour the nosejour to set
+     */
+    public void setNosejour(int[] nosejour) {
+        this.nosejour = nosejour;
+    }
+
+    /**
+     * @return the service
+     */
+    public String getService() {
+        return service;
+    }
+
+    /**
+     * @param service the service to set
+     */
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    /**
+     * @return the correspondance
+     */
+    public String getCorrespondance() {
+        return correspondance;
+    }
+
+    /**
+     * @param correspondance the correspondance to set
+     */
+    public void setCorrespondance(String correspondance) {
+        this.correspondance = correspondance;
+    }
+    
+    /**
+     * ipp
+     */
+    private int[] ipp = {};
+    /**
+     * nosejour
+     */
+    private int[] nosejour = {};
+    /**
+     * service
+     */
+    private String service = "";
+    /**
+     * correspondance
+     */
+    private String correspondance = "";
+    
     
 }
