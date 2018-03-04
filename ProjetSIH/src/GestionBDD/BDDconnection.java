b0e46f488513853c881974e28bdc27add1a4f8a0
Unknown <rome.lopes.student@gmail.com>
2018-03-04 20:50:27 +0100

    before pull

--- ProjetSIH/src/GestionBDD/BDDconnection.java
+++ ProjetSIH/src/GestionBDD/BDDconnection.java
@@ -4,48 +4,56 @@
  * and open the template in the editor.
  */
 package GestionBDD;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import org.postgresql.Driver;
 
-public class BDDconnection{
+public class BDDconnection {
 
-	/**
-	 * URL de connection
-	 */
-        private static String url = "jdbc:postgres://htdxinqp:l_YgqQRD-oDOxOC9h4732glCwj_tfSzs@horton.elephantsql.com:5432/htdxinqp";
-	/**
-	 * Nom du user
-	 */
-	private static String user = "htdxinqp";
-	/**
-	 * Mot de passe du user
-	 */
-	private static String passwd = "l_YgqQRD-oDOxOC9h4732glCwj_tfSzs";
-	/**
-	 * Objet Connection
-	 */
-	private static Connection connect;
-        
-        
-        private BDDconnection(){
-            // Nothing to do here
+    /**
+     * URL de connection
+     */
+    private static String url = "jdbc:postgres://htdxinqp:l_YgqQRD-oDOxOC9h4732glCwj_tfSzs@horton.elephantsql.com:5432/htdxinqp";
+    /**
+     * Nom du user
+     */
+    private static String user = "htdxinqp";
+    /**
+     * Mot de passe du user
+     */
+    private static String passwd = "l_YgqQRD-oDOxOC9h4732glCwj_tfSzs";
+    /**
+     * Objet Connection
+     */
+    private static Connection connect;
+
+    private BDDconnection() {
+        // Nothing to do here
+    }
+
+    /**
+     * Méthode qui va nous retourner notre instance et la créer si elle n'existe
+     * pas...
+     *
+     * @return
+     */
+    public static Connection getInstance() {
+        if (connect == null) {
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (java.lang.ClassNotFoundException e) {
+                System.out.println(e.getMessage());
+            }
+            try {
+                System.out.println(" avant essai connexion");
+                connect = DriverManager.getConnection(url, user, passwd);
+                System.out.println(" après essai connexion");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
-	
-	/**
-	 * Méthode qui va nous retourner notre instance
-	 * et la créer si elle n'existe pas...
-	 * @return
-	 */
-	public static Connection getInstance(){
-		if(connect == null){
-			try {
-				connect = DriverManager.getConnection(url, user, passwd);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}		
-		return connect;	
-	}	
+        return connect;
+    }
 }
